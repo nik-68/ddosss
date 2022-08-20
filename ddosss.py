c66fa5656bde163676282af074a9e8af02e56c47
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-20 13:44:40 +0300

    Update ddosss.py

--- ddosss.py
+++ ddosss.py
@@ -3,6 +3,7 @@ import os
 import time
 import socket
 import random
+from colorama import Fore, Back, Style
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -12,6 +13,13 @@ day = now.day
 month = now.month
 year = now.year
 
+#Colour
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
 ###################################################################################
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
